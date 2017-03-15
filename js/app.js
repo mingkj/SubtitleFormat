73e40ae05f6bbafc7f3fed779b28de1fec89d973
mingkj <yueruming@gmail.com>
2017-03-16 05:29:50 +0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,69 @@
+window.onload = function () {
+    document.getElementById("button").setAttribute("onclick", "format()");
+    document.getElementById("input").setAttribute("onkeydown", "enterkey()");
+
+    document.getElementById("file").addEventListener('change', fileSelect, false);
+}
+function enterkey() {
+    // enter 13, whitespace 20
+    if (event.keyCode == 32) { format(); }
+}
+
+// 格式化字幕文件
+function format() {
+    var subtitle = ' ';
+    // 参数的个数为 1 的时候
+    if (arguments.length === 1) {
+        subtitle = arguments[0];
+        // console.log("arguments");
+    }
+    // 无参数的时候
+    else {
+        subtitle = document.getElementById("input").value;
+        // console.log("subtile ")
+    }
+    // var subtitle = document.getElementById("input").value;
+
+    // 字幕格式 13 换行 00:00:34,042 --> 00:00:35,494
+    var text = subtitle.replace(/(\n.|\n..|\n...|.)\n[0-9][0-9]:[0-9][0-9]:[0-9][0-9],[0-9][0-9][0-9] --> [0-9][0-9]:[0-9][0-9]:[0-9][0-9],[0-9][0-9][0-9]\n/g, ' ');
+
+    //replace() 方法的参数 replacement 可以是函数而不是字符串 
+    // 合并行尾不是 . 或者 ? 的行，不替换匹配到的第一个字符
+    text = text.replace(/[^.?](\n|\n\n)/g, function (s) {
+        //  text=text.replace(/[^.?](\n|\n\n|\s\n|\s\n\n)/g,function (s)
+        return s.substring(0, 1) + ' ';
+    });
+
+    // remove more space
+    do {
+        text = text.replace('  ', ' ');
+    } while (text.indexOf('  ') !== -1);
+
+    document.getElementById("input").value = text;
+}
+
+// 文件选择
+function fileSelect(evt) {
+    var files = this.files;
+   /* var fileName = '';
+    // var html=new Array();
+    for (var i = 0, l = files.length; i < l; i++) {
+        var file = files[i];
+
+        fileName += '<p>' + file.name + ' - ' + file.size + ' bytes' + '</p>';
+
+    }
+
+    // console.log(fileName);
+    document.getElementById("file-list").innerHTML = fileName;*/
+
+    var reader = new FileReader();
+    reader.readAsText(files[0], "UTF-8");
+    reader.onload = loaded;
+}
+
+function loaded(evt) {
+    var fileString = evt.target.result;
+    format(fileString);
+}
+
